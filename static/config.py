fb3d0179adb33d0ce616204dc2ecb4f6c907aa92
Yura Yurov <48048597+Eksabit@users.noreply.github.com>
2025-04-24 16:03:32 +0300

    Add files via upload

---
+++ static/config.py
@@ -0,0 +1,9 @@
+# на этом порту будет запущен сервер
+PORT = 8888
+# версия и описание текущей прошивки
+CURRENT_FIRMWARE = {"version": 20250108, "description": 'Testovaya proshivka, dlya proverki servera kkt'}
+# версия прошивки с критическим обновлением
+CRITICAL_VERSION = 20250108
+# путь по которому нужно искать прошивки, абсолютный или относительно рабочей директориии
+FIRMWARE_PATH = "firmware"
+
